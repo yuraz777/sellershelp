750b9d6c001b0734a2941ecaf04389abb02d7f6f
yuraz777 <yuraz777@gmail.com>
2026-03-21 01:02:45 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,6 +6,21 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.sellershelp.ru',
+          },
+        ],
+        destination: 'https://sellershelp.ru/:path*',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig

fffa5628dd4b62abe6775f1b1bf499e2b4cfaf33
yuraz777 <yuraz777@gmail.com>
2026-04-13 13:41:07 +0300

    Fix Metrika SSR init

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,6 +57,7 @@ export default function RootLayout({
             k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
             (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
             ym(108517724, "init", {
+              ssr: true,
               clickmap: true,
               trackLinks: true,
               accurateTrackBounce: true,

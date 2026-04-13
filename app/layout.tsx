c97ab09a87fb563e9a1a4954aa674c4279f908cf
yuraz777 <yuraz777@gmail.com>
2026-04-13 13:27:12 +0300

    Update Metrika counter ID

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,7 +56,7 @@ export default function RootLayout({
             for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r){return;}}
             k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
             (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-            ym(93877489, "init", {
+            ym(108517724, "init", {
               clickmap: true,
               trackLinks: true,
               accurateTrackBounce: true,
@@ -66,7 +66,7 @@ export default function RootLayout({
         </Script>
         <noscript>
           <div>
-            <img src="https://mc.yandex.ru/watch/93877489" style={{position:'absolute', left:'-9999px'}} alt="" />
+            <img src="https://mc.yandex.ru/watch/108517724" style={{position:'absolute', left:'-9999px'}} alt="" />
           </div>
         </noscript>
       </body>
